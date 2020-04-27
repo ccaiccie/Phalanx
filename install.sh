29f5b60ab5310c5bdbef829b8b9e60f4eaacb14f
CJ <11094223+cplmayo@users.noreply.github.com>
2020-04-27 17:40:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,28 @@
-#!/bin/bash
+!/bin/bash
 # Phalanx Install and setup script.
 
-if [ ! "$userid" = "0" ]; then
-   echo "You have to run this script as root. eg."
-   echo "  sudo install.sh"
-   echo "Exiting."
-   echo ${LINE}
-   exit 9
-else
-   do_log "Check OK: User-ID is ${userid}."
-fi
-
-if [ "$ID" == "ubuntu" ] ; then
-   dist='apt'
-   distversion="ubuntu"
-fi
-
-if [ "$dist" == "apt" ]; then
-   echo "Updating package manager"
-   run 'apt update'
-   echo "Updating package manager"
-   run 'apt upgrade -y'
-   run '/usr/local/bin/python3 -m pip install pip3'
-   run 'pip3 install requests'
-   run 'pip3 install ipsetpy'
-   run 'git clone https://github.com/sanyi/ipsetpy.git'
-   run 'rm /usr/local/lib/python3.6/dist-packages/ipsetpy/wrapper.py'
-   run 'cp ipsetpy/wrapper.py /usr/local/lib/python3.6/dist-packages/ipsetpy/wrapper.py'
-   run 'rm -R ipsetpy'
-   run 'mv iprange /usr/local/bin/iprange'
-   run 'mkdir /opt/phalanx'
-   run 'mv *.py /opt/phalanx'
-   run 'mv *.py /opt/phalanx'
-   run 'python3 /opt/phalanx/main.py -s'
-   run 'python3 /opt/phalanx/main.py -u'
-   run ' ln -s /opt/phalanx/main.py /usr/local/bin/main.py'
-   run 'cp phalanx-startup.service /lib/systemd/system'
-   run 'systemctl daemon-reload'
-   run 'systemctl enable phalanx-startup.service'
-fi
+echo "Updating package manager"
+run 'apt update'
+echo "Updating package manager"
+apt upgrade -y
+apt install -y -q
+python3-pip
+pip3 install requests
+pip3 install ipsetpy
+git clone https://github.com/sanyi/ipsetpy.git
+rm /usr/local/lib/python3.6/dist-packages/ipsetpy/wrapper.py
+cp ipsetpy/wrapper.py /usr/local/lib/python3.6/dist-packages/ipsetpy/wrapper.py
+rm -R ipsetpy
+mv iprange /usr/local/bin/iprange
+mkdir /opt/phalanx
+mv *.py /opt/phalanx
+mv *.json /opt/phalanx
+python3 /opt/phalanx/main.py -S
+python3 /opt/phalanx/main.py -u
+ln -s /opt/phalanx/main.py /usr/local/bin/main.py
+phalanx-startup.service /lib/systemd/system
+systemctl daemon-reload
+systemctl enable phalanx-startup.service
 
 echo "Install complete!"
 echo "To update the block list run python3 main.py -u"
